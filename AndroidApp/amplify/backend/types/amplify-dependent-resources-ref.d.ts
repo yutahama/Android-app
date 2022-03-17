b93c277ede3bed5ded31a28d8f072c38da1f4dc9
https://github.com/yutahama/kitkat.git <bs120029@sun.ac.jp>
2022-03-17 12:59:34 +0900

    add amplify-api

--- AndroidApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ AndroidApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "androidapp4449ded0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
     }
 }
